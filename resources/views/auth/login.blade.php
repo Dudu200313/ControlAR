a2bff6d2e44b85e9cd143938f7dc62a7d482f340
CarolayneMR <carolwolf0101@gmail.com>
2023-10-19 01:59:09 -0300

    Organizando um pouco a tela Inicial e Cadastro.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,34 @@
+<nav class="bg-blue-200 p-4 flex justify-between items-center">
+    <a href="nadinha" class="text-white text-4xl font-extrabold tracking-wider">
+        Control<span class="text-blue-600">AR</span>
+    </a>
+    <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+        Saiba-mais
+    </button>
+</nav>
+
+
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
         </x-slot>
 
-        <x-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+        <div class="flex items-center">
+            <div class="">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-16 h-16 ml-2 mb-2">
+                    <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                </svg>
             </div>
-        @endif
+            <h1 class="text-3xl font-bold text-center">Login</h1>
+        </div>
 
+
+        <x-validation-errors class="mb-4" />
+        @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
+        </div>
+        @endif
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
@@ -21,27 +38,27 @@
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <x-label for="password" value="{{ __('Senha') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Manter conectado') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Esqueceu da senha?') }}
+                </a>
                 @endif
 
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-button>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-lg ml-2">
+                    {{ __('Conectar') }}
+                </button>
             </div>
         </form>
     </x-authentication-card>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,8 +6,7 @@
 
 <x-guest-layout>
     <x-authentication-card>
-        <x-slot name="logo">
-            
+        <x-slot name="logo">    
         </x-slot>
 
         <x-validation-errors class="mb-4" />
@@ -16,12 +15,12 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
             <div class="flex items-start ">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-    <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
-</svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-16 h-16 ml-2 mb-2">
+                    <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                </svg>
 
-                 <h1 class="text-2xl ml-21">Cadatrase</h1>
-           </div>
+                <h1 class="text-2xl ml-21">Cadastrar-se</h1>
+            </div>
 
             <div>
                 <x-label for="nome" value="{{ __('Nome') }}" />
@@ -34,8 +33,8 @@
             </div>
 
             <div class="mt-4">
-                <x-label for="senha" value="{{ __('Senha') }}" />
-                <x-input id="senha" class="block mt-1 w-full" type="senha" name="senha" required autocomplete="Nova-senha" />
+                <x-label for="password" value="{{ __('Senha') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
 
@@ -57,12 +56,12 @@
             @endif
 
             <div class="flex flex-col texte-center items-center mt-4">
-  <button class="bg-blue-400 w-48 mb-4 text-center text-white">
-    {{ __('Registro-se') }}
+  <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-lg ml-2">
+    {{ __('Registrar-se') }}
   </button>
 
   <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-    {{ __('Já registrado?') }}
+    {{ __('Já possui uma conta?') }}
   </a>
 </div>
 
