00faf1fad269ac577c589ddb7c9d5388e7283c67
ianq1w1 <ianpereiraian2@gmail.com>
2023-12-15 23:12:17 -0300

    correção de bug no delete e da dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,9 @@
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 cursor-pointer hover:text-red-500">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
-                                <button>
+                                </button>
+                            </form>
+
                         </div>
                     </div>
 
